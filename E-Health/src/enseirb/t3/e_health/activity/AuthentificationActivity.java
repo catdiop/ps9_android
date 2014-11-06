ec6d870ea15e450b760aa3eac26f50d19bff6bab
aymeric_pocreau <15635413>
2014-11-06 16:15:42 +0100

    Ajout du module d'activation du bluetooth sur la tablette via un bouton adéquat

--- E-Health/src/enseirb/t3/e_health/activity/AuthentificationActivity.java
+++ E-Health/src/enseirb/t3/e_health/activity/AuthentificationActivity.java
@@ -2,49 +2,68 @@ package enseirb.t3.e_health.activity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.project.e_health.R;
 
 import enseirb.t3.e_health.DAO.UserDatabaseHandler;
+import enseirb.t3.e_health.bluetooth.Bluetooth;
 
 /**
  * @author catdiop
  *
  */
-public class AuthentificationActivity extends Activity {
+public class AuthentificationActivity extends Activity implements OnClickListener {
+	
+	private final static int  REQUEST_ENABLE_BT = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		//Hide the status Bar
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_authentification);
+		
+		Button bluetooth = (Button) findViewById(R.id.bluetooth);		
+		bluetooth.setOnClickListener(this);
+		
+		Button connexion = (Button) findViewById(R.id.connexion);
+		connexion.setOnClickListener(this);
 	}
 
-
-
+	@Override
 	public void onClick(View v) {
-
-		//Si l'utilisateur appuit sur connexion
-		if(v.getId()==R.id.connexion) {
-			UserDatabaseHandler dbHandler=new UserDatabaseHandler(this.getApplicationContext());
-			TextView view1=(TextView)findViewById(R.id.username);
-			TextView view2=(TextView)findViewById(R.id.password);
-			if(dbHandler.isUser(view1.getText().toString(), view2.getText().toString())) {
-				//on se connecte
-			}
-			else
-				createDialog("Le nom d'utilisateur ou le mot de passe est incorrect");
-			}
-			
+		
+		switch (v.getId()) {
+		
+			//Si l'utilisateur appuit sur connexion
+			case R.id.connexion:
+				UserDatabaseHandler dbHandler=new UserDatabaseHandler(this.getApplicationContext());
+				TextView view1=(TextView)findViewById(R.id.username);
+				TextView view2=(TextView)findViewById(R.id.password);
+				if(dbHandler.isUser(view1.getText().toString(), view2.getText().toString())) {
+					//on se connecte
+				}
+				else {
+						createDialog("Le nom d'utilisateur ou le mot de passe est incorrect");
+					}
+				break;
+	
+			//Si l'utilisateur active le bluetooth
+			case R.id.bluetooth:
+				Bluetooth bluetooth = new Bluetooth(this);
+				bluetooth.enableBluetooth();
+				
+				break;
 		}
+	}
 	
 	private void createDialog(String msg) {
 
--- E-Health/src/enseirb/t3/e_health/entity/Doctor.java
+++ E-Health/src/enseirb/t3/e_health/entity/Doctor.java
@@ -4,11 +4,11 @@ public class Doctor {
 
 	private int id;
 	private User user;
+	private String password;
 
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -16,8 +16,15 @@ public class Doctor {
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
