7d3dbc3c7bffef2033eb97d5d775ba4c91edc08f
aymeric_pocreau <15635413>
2014-11-10 12:00:49 +0100

    blabla

--- E-Health/src/enseirb/t3/e_health/bluetooth/Bluetooth.java
+++ E-Health/src/enseirb/t3/e_health/bluetooth/Bluetooth.java
@@ -6,9 +6,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 
 public class Bluetooth {
 	
@@ -18,7 +16,6 @@ public class Bluetooth {
 	private Set<BluetoothDevice> pairedDevices;
 	private BroadcastReceiver mReceiver = null;
     private Set<BluetoothDevice> mArrayAdapter;
-	
 	public Bluetooth (Activity activity) {
 		this.activity = activity;
 	}
@@ -34,23 +31,11 @@ public class Bluetooth {
 	public void DiscoverDevices() {
 	
 		// Create a BroadcastReceiver for ACTION_FOUND
-		mReceiver = new BroadcastReceiver() {
-		    public void onReceive(Context context, Intent intent) {
-		        String action = intent.getAction();
-		        // When discovery finds a device
-		        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-		            // Get the BluetoothDevice object from the Intent
-		            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-					// Add the name and address to an array adapter to show in a ListView
-		            mArrayAdapter.add(device);
-		            System.out.println(device.getName());
-		        }
-		    }
-		};
+		mReceiver = new BluetoothBroadcastReceiver();
 		// Register the BroadcastReceiver
-		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-		this.activity.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+		//IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		//this.activity.registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
 		
-		mBluetoothAdapter.startDiscovery();
+		boolean b=mBluetoothAdapter.startDiscovery();
 	}
 }
