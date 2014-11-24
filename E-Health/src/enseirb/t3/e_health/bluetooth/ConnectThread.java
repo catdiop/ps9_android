194707ad680021ec7122f37f7bf26802d223fcd2
aymeric_pocreau <15635413>
2014-11-24 15:41:19 +0100

    Gestion du bluetooth handler

--- E-Health/src/enseirb/t3/e_health/bluetooth/ConnectThread.java
+++ E-Health/src/enseirb/t3/e_health/bluetooth/ConnectThread.java
@@ -1,18 +1,25 @@
 package enseirb.t3.e_health.bluetooth;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 public class ConnectThread extends Thread {
 
+	private static final int MESSAGE_READ = 999;
 	private  UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 	private BluetoothSocket mmSocket = null;
 	private final BluetoothDevice mmDevice;
+	private InputStream mmInStream;
+	private OutputStream mmOutStream;
 	
 	public ConnectThread(BluetoothDevice device){
 		Log.d("device name", device.getName());
@@ -27,16 +34,34 @@ public class ConnectThread extends Thread {
 		}
 		catch(Exception e){
 		}
+		
+		try {
+			mmInStream=mmSocket.getInputStream();
+			mmOutStream=mmSocket.getOutputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 //		mmSocket=tmp;
 	}
 
 	@Override
 	public void run() {
+		byte[] buffer= new byte[4096]; //buffer store for the stream
+		int bytes = 0;
 		try{
 			BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
 			if (!mmSocket.isConnected()) {
 				mmSocket.connect();
 				Log.d("Status", "Arduino connecté");
+				while (true) {
+					bytes=mmInStream.read(buffer);
+					String value = new String(buffer, "UTF-32");
+					Log.d("buffer", value);
+					mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+	                .sendToTarget();
+				}
 			}
 			else
 				Log.d("Status", "Arduino déjà connecté");
@@ -48,8 +73,9 @@ public class ConnectThread extends Thread {
 			catch(IOException closeException){return;}
 		} 
 		//Do work to manage the connection(in a separate thread)
-		ReadThread readthread = new ReadThread(mmSocket);
-		readthread.start();
+		
+//		ReadThread readthread = new ReadThread(mmSocket);
+//		readthread.start();
 	}
 	
 	public void cancel(){
@@ -57,4 +83,20 @@ public class ConnectThread extends Thread {
 			mmSocket.close();
 		} catch(IOException e) {};
 	}
+	
+	static Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			byte[] writeBuf = (byte[]) msg.obj;
+			int begin = (int) msg.arg1;
+			int end = (int) msg.arg2;
+
+			switch (msg.what) {
+			case MESSAGE_READ:
+			      // your code goes here
+				Log.d("handler", "message read");
+				break;
+			}
+		}
+	};
 }
--- E-Health/src/enseirb/t3/e_health/bluetooth/ReadThread.java
+++ E-Health/src/enseirb/t3/e_health/bluetooth/ReadThread.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 
 public class ReadThread extends Thread {
@@ -12,18 +15,20 @@ public class ReadThread extends Thread {
 	private static final int MESSAGE_READ = 999;
 	private InputStream mmInStream;
 	private OutputStream mmOutStream;
+	private BluetoothSocket mmSocket;
 	
 	public ReadThread(BluetoothSocket mmSocket) {
 		
 		try{
 			mmInStream=mmSocket.getInputStream();
 			mmOutStream=mmSocket.getOutputStream();
+			this.mmSocket = mmSocket;
 		}
 		catch(IOException e) {}
 	}
 	
 	public void run() {
-		byte[] buffer= new byte[1024]; //buffer store for the stream
+		byte[] buffer= new byte[4]; //buffer store for the stream
 		int bytes = 0;  //bytes returned from read
 		
 		//keep listening to the InputStream until an exceptions occurs
@@ -31,13 +36,14 @@ public class ReadThread extends Thread {
 			//read from InputStream
 				try {
 					bytes=mmInStream.read(buffer);
-					Log.d("buffer", buffer.toString());
+					String value = new String(buffer, "UTF-8");
+					Log.d("buffer", value);
 					
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-//				mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
-//                .sendToTarget();
+				//mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                //.sendToTarget();
 			Log.d("buffer", buffer.toString());
 		}
 	}
@@ -48,21 +54,29 @@ public class ReadThread extends Thread {
         } catch (IOException e) { }
     }
 	
-//	Handler mHandler = new Handler() {
-//		@Override
-//		public void handleMessage(Message msg) {
-//			byte[] writeBuf = (byte[]) msg.obj;
-//			int begin = (int) msg.arg1;
-//			int end = (int) msg.arg2;
-//
-//			switch (msg.what) {
-//			case 1:
-//				String writeMessage = new String(writeBuf);
-//				writeMessage = writeMessage.substring(begin, end);
-//				break;
-//			}
-//		}
-//	};
+	public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e("cancel", "close() of connect socket failed", e);
+        }
+    }
+	
+	static Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			byte[] writeBuf = (byte[]) msg.obj;
+			int begin = (int) msg.arg1;
+			int end = (int) msg.arg2;
+
+			switch (msg.what) {
+			case MESSAGE_READ:
+			      // your code goes here
+				//Log.d("handler", "message read");
+				break;
+			}
+		}
+	};
 	
 }
 
