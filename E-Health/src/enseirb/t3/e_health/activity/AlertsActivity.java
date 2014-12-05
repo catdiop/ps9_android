6b0b506a0c57ec29828f891e2f912a6a869ed939
catdiop <diop.cheikhahmed@gmail.com>
2014-12-05 16:29:30 +0100

    pop up ajout patient+menu liste alerte et bluetooth

--- E-Health/src/enseirb/t3/e_health/activity/AlertsActivity.java
+++ E-Health/src/enseirb/t3/e_health/activity/AlertsActivity.java
@@ -5,9 +5,14 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,12 +21,14 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.project.e_health.R;
 
 import enseirb.t3.e_health.entity.Alert;
 import enseirb.t3.e_health.entity.Patient;
+import enseirb.t3.e_health.entity.User;
 
 public class AlertsActivity extends Activity {
 
@@ -35,6 +42,9 @@ public class AlertsActivity extends Activity {
 		switch (item.getItemId()) {
 		case android.R.id.home:
 			NavUtils.navigateUpFromSameTask(this);
+			break;
+		case R.id.add_patient:
+			add();
 			break;
 		default:;	
 		}
@@ -68,8 +78,8 @@ public class AlertsActivity extends Activity {
 			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
 				// TODO Auto-generated method stub
-			
-				
+
+
 				return false;
 			}
 
@@ -77,17 +87,15 @@ public class AlertsActivity extends Activity {
 		List<Alert> alerts=new LinkedList<Alert>();
 		Calendar c=Calendar.getInstance();
 		Patient p=new Patient();
-		p.setFirstname("lili");
-		p.setLastname("lolo");
+		p.setFirstname("titi");
+		p.setLastname("toto");
 		Alert a=new Alert(p, c.getTime(), "Tachycardie");
 		a.setPatient(p);
 		a.setDate(c.getTime());
-//		alerts.add(a);
-		p.setFirstname("titi");
-		p.setLastname("toto");
+		//		alerts.add(a);
 		a.setPatient(p);
 		alerts.add(a);
-		
+
 		adapter=new ListAlertAdapter(AlertsActivity.this, alerts);
 		list.setAdapter(adapter);
 	}
@@ -97,7 +105,137 @@ public class AlertsActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu items for use in the action bar
 		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.authentificate, menu);
+		inflater.inflate(R.menu.alerts, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+
+	//ajout d'un patient
+	//add new feed
+	private void add() {
+		LayoutInflater inflater = LayoutInflater.from(this);
+		View addPatient = inflater.inflate(R.layout.activity_patient_registration,
+				null);
+
+		final DialogWrapper wrapper = new DialogWrapper(addPatient);
+
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.menu_add_patient)
+		.setView(addPatient)
+		.setPositiveButton(R.string.register,
+				new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,
+					int whichButton) {
+				processAdd(wrapper);
+			}
+		})
+		.setNegativeButton(R.string.cancel,
+				new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,
+					int whichButton) {
+				// ignore, just dismiss
+			}
+		}).show();
+	}
+
+	@SuppressWarnings("deprecation")
+	private void processAdd(DialogWrapper wrapper) {
+		addPatient(wrapper.getUsername(), wrapper.getPassword(), wrapper.getConfirmPwd());
+	}
+	class DialogWrapper {
+		EditText username = null;
+		EditText password = null;
+		EditText confirmPwd = null;
+		View base = null;
+
+		DialogWrapper(View base) {
+			this.base = base;
+			this.username = (EditText) base.findViewById(R.id.username);
+			this.password = (EditText) base.findViewById(R.id.password);
+			this.confirmPwd = (EditText) base.findViewById(R.id.passwordCheck);
+		}
+
+		String getUsername() {
+			return (getUsernameField().getText().toString());
+		}
+
+		String getPassword() {
+			return (getPasswordField().getText().toString());
+		}
+
+		String getConfirmPwd() {
+			return (getConfirmPwdField().getText().toString());
+		}
+
+
+		private EditText getUsernameField() {
+			if (this.username == null) {
+				this.username = (EditText) base.findViewById(R.id.username);
+			}
+
+			return (this.username);
+		}
+
+		private EditText getPasswordField() {
+			if (this.password == null) {
+				this.password = (EditText) base.findViewById(R.id.password);
+			}
+
+			return (this.password);
+		}
+
+		private EditText getConfirmPwdField() {
+			if (this.confirmPwd == null) {
+				this.confirmPwd = (EditText) base.findViewById(R.id.passwordCheck);
+			}
+
+			return (this.confirmPwd);
+		}
+	}
+
+	private void addPatient(String username, String password, String confirmPwd){
+		if (username.length() > 0 && password.length() > 0 && confirmPwd.length() >0) {
+
+			// If user's name is already being used
+			if (EHealth.db.doesUserExist(username)) {
+
+				createDialog("Username already in use.");
+				//				Intent intent = new Intent(AuthentificationActivity.this, Graph.class);
+				//				startActivity(intent);
+			} else {
+
+				// Check password
+				if (password.equals(confirmPwd.toString())) {
+
+					Patient patient = new Patient(username, password);
+					patient.setType("patient");
+					EHealth.db.createUser(patient);
+
+					createDialog("Patient account created !");
+				}
+				else {
+
+					createDialog("Passwords don't match.");
+				}
+			}
+		}
+		else {
+
+			createDialog("Empty field(s).");
+		}
+	}
+
+	private void createDialog(String msg) {
+
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setMessage(msg);
+		dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		dialog.show();
 	}
 }
--- E-Health/src/enseirb/t3/e_health/activity/AuthentificationActivity.java
+++ E-Health/src/enseirb/t3/e_health/activity/AuthentificationActivity.java
@@ -5,6 +5,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,13 +27,44 @@ import enseirb.t3.e_health.entity.Patient;
 public class AuthentificationActivity extends Activity implements OnClickListener {
 
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.authentificate, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			break;
+			
+		case R.id.menu_bluetooth:
+			Bluetooth bluetooth = new Bluetooth(this);
+			bluetooth.enableBluetooth();
+			if (!bluetooth.queryingPairedDevices()) {
+				// discover
+				bluetooth.discoverDevices();
+			}
+			break;
+			
+		case R.id.menu_alerts:
+			Intent intent = new Intent(AuthentificationActivity.this, AlertsActivity.class);
+			startActivity(intent);
+			break;
+			
+		default:;	
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_authentification);
-
-		Button bluetooth = (Button) findViewById(R.id.bluetooth);
-		bluetooth.setOnClickListener(this);
 
 		Button connexion = (Button) findViewById(R.id.connexion);
 		connexion.setOnClickListener(this);
@@ -38,21 +73,21 @@ public class AuthentificationActivity extends Activity implements OnClickListene
 		ArduinoData aData = new ArduinoData("DATA200000;180000|B|85;181000|B|90;182000|B|105;183000|B|100;184000|B|95;185000|B|98;DATA220000;186000|B|95;187000|B|95;188000|B|95;189000|B|92;190000|B|85;191000|B|90;192000|B|105");
 
 		// Get data and store it to the db
-		
+
 		aData.getAndStoreData();
-//		ArduinoData aData = new ArduinoData("DATA200000;180000|B|85;181000|B|90;182000|B|105;183000|B|100;184000|B|95;185000|B|98;186000|B|95;187000|B|95;188000|B|95;189000|B|92;190000|B|85;191000|B|90;192000|B|105;193000|B|100;194000|B|95;195000|B|98;196000|B|95;197000|B|95;198000|B|95;199000|B|92");
-		
-//		String aDataStr = aData.getArduinoData();
+		//		ArduinoData aData = new ArduinoData("DATA200000;180000|B|85;181000|B|90;182000|B|105;183000|B|100;184000|B|95;185000|B|98;186000|B|95;187000|B|95;188000|B|95;189000|B|92;190000|B|85;191000|B|90;192000|B|105;193000|B|100;194000|B|95;195000|B|98;196000|B|95;197000|B|95;198000|B|95;199000|B|92");
+
+		//		String aDataStr = aData.getArduinoData();
 
 		// Split data
-//		String[] chunks = aData.getChunks(aDataStr);
-//		aData.stockData(chunks, dbHandler);
+		//		String[] chunks = aData.getChunks(aDataStr);
+		//		aData.stockData(chunks, dbHandler);
 
 		//tests r�cuperation
-//		List<Data> DataList = dbHandler.retrieveDataList("B");
-//		Log.d("data", DataList.get(1).getDataname());
-//		Log.d("data", DataList.get(1).getValue());
-//		Log.d("data", DataList.get(1).getDate());
+		//		List<Data> DataList = dbHandler.retrieveDataList("B");
+		//		Log.d("data", DataList.get(1).getDataname());
+		//		Log.d("data", DataList.get(1).getValue());
+		//		Log.d("data", DataList.get(1).getDate());
 
 		// Get data timestamp
 		//		String date = aData.getDataTimestamp(chunks[0]);
@@ -72,23 +107,13 @@ public class AuthentificationActivity extends Activity implements OnClickListene
 			TextView usernameView = (TextView) findViewById(R.id.username);
 			TextView passwordView = (TextView) findViewById(R.id.password);
 			//if (dbHandler.isUser(usernameView.getText().toString(), passwordView.getText()
-				//	.toString())) {
-				// on se connecte
-				Intent intent = new Intent(AuthentificationActivity.this, Graph.class);
-				startActivity(intent);
+			//	.toString())) {
+			// on se connecte
+			Intent intent = new Intent(AuthentificationActivity.this, Graph.class);
+			startActivity(intent);
 			//} else {
-				//createDialog("Le nom d'utilisateur ou le mot de passe est incorrect");
+			//createDialog("Le nom d'utilisateur ou le mot de passe est incorrect");
 			//}
-			break;
-
-			// Si l'utilisateur active le bluetooth
-		case R.id.bluetooth:
-			Bluetooth bluetooth = new Bluetooth(this);
-			bluetooth.enableBluetooth();
-			if (!bluetooth.queryingPairedDevices()) {
-				// discover
-				bluetooth.discoverDevices();
-			}
 			break;
 		}
 	}
