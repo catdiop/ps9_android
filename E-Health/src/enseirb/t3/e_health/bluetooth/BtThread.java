e3fdc0db228e206bd86209e83286a9c69a5fdaa3
rscimia <romainscimia@gmail.com>
2014-12-05 15:23:55 +0100

    Ajout de la fonction d'envoi de message bluetooth

--- E-Health/src/enseirb/t3/e_health/bluetooth/BtThread.java
+++ E-Health/src/enseirb/t3/e_health/bluetooth/BtThread.java
@@ -3,53 +3,39 @@ package enseirb.t3.e_health.bluetooth;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.UUID;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
-public class ConnectThread extends Thread {
+public class BtThread extends Thread {
 
-	private Context context;
-	private static final int MESSAGE_READ = 999;
-	private  UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 	private BluetoothSocket mmSocket = null;
-	private final BluetoothDevice mmDevice;
 	private InputStream mmInStream;
 	private OutputStream mmOutStream;
 	private Handler handler;
 	
 	private String TAG = "ConnectThread";
 	
-	public ConnectThread(BluetoothDevice device, Context context, Handler handler){
-		this.context = context;
+	public BtThread(BluetoothDevice device, Handler handler){
 		this.handler = handler;
 		Log.d(TAG, device.getName());
-		BluetoothSocket tmp = null;
-		mmDevice=device;
-		
 		
 		//get a BluetoothSocket to connect with the given BluetoothDevice
 		try{
-//			mmSocket = device.createRfcommSocketToServiceRecord(SERIAL_UUID);
-//			Method m=mmDevice.getClass().getMethod("createInsecureRfcommSocket", new Class[] {int.class});
-//			tmp=(BluetoothSocket)m.invoke(mmDevice, Integer.valueOf(1));
 			mmSocket = (BluetoothSocket) device.getClass().getMethod("createInsecureRfcommSocket", new Class[] {int.class}).invoke(device,1);
 		}
 		catch(Exception e){
 		}
 		
 		try {
-			mmInStream=mmSocket.getInputStream();
-			mmOutStream=mmSocket.getOutputStream();
+			mmInStream = mmSocket.getInputStream();
+			mmOutStream = mmSocket.getOutputStream();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -97,10 +83,21 @@ public class ConnectThread extends Thread {
 		} 
 	}
 	
+	/**
+     * Write to the connected OutStream.
+     * @param buffer  The bytes to write
+     */
+    public void write(byte[] buffer) {
+        try {
+            mmOutStream.write(buffer);
+        } catch (IOException e) {
+            Log.e(TAG, "Exception during write", e);
+        }
+    }
+	
 	public void cancel(){
 		try{
 			mmSocket.close();
 		} catch(IOException e) {};
 	}
-	
 }
