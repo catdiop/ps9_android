fe4092dbb58e4d0f3b2fef14b568b5651adfd0d8
Guillaume Kermorgant <gkermorgant@enseirb.fr>
2014-12-22 18:17:48 +0100

    - Ajout d'un splash screen

---
+++ E-Health/src/enseirb/t3/e_health/activity/SplashScreen.java
@@ -0,0 +1,50 @@
+package enseirb.t3.e_health.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.project.e_health.R;
+
+public class SplashScreen extends Activity {
+
+	/** Durée d'affichage du SplashScreen */
+	   protected int _splashTime = 3000; 
+
+	   private Thread splashTread;
+
+	   /** Chargement de l'Activity */
+	   @Override
+	   public void onCreate(Bundle savedInstanceState) 
+	   {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.activity_splash_screen);
+
+	      final SplashScreen sPlashScreen = this; 
+
+	      /** Thread pour l'affichage du SplashScreen */
+	      splashTread = new Thread() 
+	      {
+	         @Override
+	         public void run() 
+	         {
+	            try 
+	            {
+	                 synchronized(this)
+	                 {
+	                    wait(_splashTime);
+	                 }
+	             } catch(InterruptedException e) {} 
+	             finally 
+	             {
+	                finish();
+	                Intent i = new Intent();
+	                i.setClass(sPlashScreen, AuthentificationActivity.class);
+	                startActivity(i);
+	             }
+	          }
+	       };
+
+	       splashTread.start();
+	    }
+}
